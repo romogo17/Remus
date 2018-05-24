7e790cc851b6f7b66e752038c19718c3743a82b5
Roberto <romogo17@gmail.com>
2018-05-23 19:48:25 -0600

    Changed d image

--- app/src/main/java/com/una/optimizeprime/remus/Database.java
+++ app/src/main/java/com/una/optimizeprime/remus/Database.java
@@ -1,15 +1,12 @@
 package com.una.optimizeprime.remus;
 
 
-import android.provider.ContactsContract;
 import android.util.Log;
 
-import com.google.android.gms.common.data.DataBufferObserver;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
